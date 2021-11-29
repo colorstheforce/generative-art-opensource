b0ca3fb60540fe1c85111653107878dc0b6bfa44
ColorsTheForce <66538158+colorstheforce@users.noreply.github.com>
2021-11-28 22:26:43 -0500

    REplaced Entered
    Replaced, Entered Script, Developed Art

--- input/config.js
+++ input/config.js
@@ -119,7 +119,7 @@ const width = 1000;
 // image height in pixels
 const height = 1000;
 // description for NFT in metadata file
-const description = "This is an NFT made by the coolest generative code.";
+const description = "This is an NFT made by StopTrippin, ColorsTheForce, and is the coolest generative code art in the Universe.";
 // base url to use in metadata file
 // the id of the nft will be added to this url, in the example e.g. https://hashlips/nft/1 for NFT with id 1
 const baseImageUri = "https://hashlips/nft";
